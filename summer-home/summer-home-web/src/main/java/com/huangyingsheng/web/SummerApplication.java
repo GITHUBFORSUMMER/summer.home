a4ff029bc7ea297aaade802aff0ce955b556cd5e
huangyingsheng <huangyingsheng@yiguo.com>
2019-02-22 17:32:50 +0800

    up

--- summer-home/summer-home-web/src/main/java/com/huangyingsheng/web/SummerApplication.java
+++ summer-home/summer-home-web/src/main/java/com/huangyingsheng/web/SummerApplication.java
@@ -1,13 +1,17 @@
 package com.huangyingsheng.web;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.huangyingsheng.web.dao")
 public class SummerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SummerApplication.class, args);
     }
+
 
 }
---
+++ summer-home/summer-home-web/src/main/java/com/huangyingsheng/web/controller/HomeController.java
@@ -0,0 +1,43 @@
+package com.huangyingsheng.web.controller;
+
+import com.huangyingsheng.web.model.vo.TokenVo;
+import com.huangyingsheng.web.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    private String appId = "wx80a7631fb6f3e4d2";
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String summer(Model model) {
+        String requestURL = httpServletRequest.getRequestURL().toString();
+        String queryString = httpServletRequest.getQueryString();
+        if (queryString != null && !queryString.isEmpty()) {
+            requestURL = requestURL + "?" + httpServletRequest.getQueryString();
+        }
+        System.out.println(requestURL);
+        TokenVo wxJsConfig = tokenService.getWxJsConfig(requestURL, appId);
+        model.addAttribute("wxjsconfig", wxJsConfig);
+        return "summer";
+    }
+
+    @GetMapping("/get")
+    @ResponseBody
+    public String get() {
+        return "123";
+    }
+
+}
